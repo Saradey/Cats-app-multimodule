3dc79a6021bac88e6ac626a4186d50c78025d3e6
Evgeny Goncharov <Evgenii.Goncharov@excelian.com>
2020-10-20 02:01:12 +0300

    откомментировали модуль core

--- sources/core/src/main/java/com/evgeny/goncharov/core/CoreProviderFactory.kt
+++ sources/core/src/main/java/com/evgeny/goncharov/core/CoreProviderFactory.kt
@@ -8,20 +8,38 @@ import com.evgeny.goncharov.coreimpl.database.DaggerDatabaseComponent
 import com.evgeny.goncharov.coreimpl.mangers.di.DaggerManagersComponent
 import com.evgeny.goncharov.coreimpl.network.DaggerNetworkComponent
 
+/**
+ * Фектори для создания даггер графов
+ */
 object CoreProviderFactory {
 
+    /**
+     * Создаем даггер граф базы данных
+     * @param contextProvider AppComponent для провайда контекста
+     * @return контракт для провайда баз данных
+     */
     fun createDatabaseComponent(contextProvider: ContextProvider): DatabaseProvider {
         return DaggerDatabaseComponent.builder()
             .contextProvider(contextProvider)
             .build()
     }
 
+    /**
+     * Создаем даггер граф для сетевого слоя
+     * @param contextProvider AppComponent для провайда контекста
+     * @return конракт для провайда api
+     */
     fun createNetworkComponent(contextProvider: ContextProvider): NetworkProvider {
         return DaggerNetworkComponent.builder()
             .contextProvider(contextProvider)
             .build()
     }
 
+    /**
+     * Создаем даггер граф для провайда менеджеров
+     * @param contextProvider AppComponent для провайда контекста
+     * @return контракт для провайда менеджеров
+     */
     fun createManagerComponent(contextProvider: ContextProvider): ManagerProvider {
         return DaggerManagersComponent.builder()
             .contextProvider(contextProvider)
