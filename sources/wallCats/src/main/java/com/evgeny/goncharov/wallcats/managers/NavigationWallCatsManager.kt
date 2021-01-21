3dba00b76a614a0d384df20c2aed9a298e8fc590
Evgeny Goncharov <Evgenii.Goncharov@excelian.com>
2021-01-21 21:58:07 +0300

    task-22 refactoring

--- sources/wallCats/src/main/java/com/evgeny/goncharov/wallcats/managers/NavigationWallCatsManager.kt
+++ sources/wallCats/src/main/java/com/evgeny/goncharov/wallcats/managers/NavigationWallCatsManager.kt
@@ -7,9 +7,22 @@ import androidx.fragment.app.FragmentManager
  */
 interface NavigationWallCatsManager {
 
+    /**
+     * Перейти на стену описания кота
+     * @param id айдишник кота
+     * @param requireFragmentManager для проброски в медиатор
+     */
     fun goToTheScreenCatDescription(id: String, requireFragmentManager: FragmentManager)
 
+    /**
+     * Перейти на стену поиска котов
+     * @param requireFragmentManager для проброски в медиатор
+     */
     fun goToTheSearchScreen(requireFragmentManager: FragmentManager)
 
+    /**
+     * Перейти на экран настроек
+     * @param requireFragmentManager для проброски в медиатор
+     */
     fun goToTheSettingsScreen(requireFragmentManager: FragmentManager)
 }
